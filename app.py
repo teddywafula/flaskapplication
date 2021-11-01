28e5404f90239e117b9f87538b5815b31b310d68
teddywafula <teddywafula@gmail.com>
2021-11-01 10:51:03 +0300

    add get post requests

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "test"
 
+@app.route("/testmethods", methods = ["POST","GET"])
+def api_post_get_test_methods():
+    if request.method == "POST":
+        return "post request"
+    if request.method == "GET":
+        return "get request"
 if __name__ == "__main__":
     app.run()
